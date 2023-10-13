079266518890cc8406126b04add9d8bcfc286afe
Pixel2203 <66052827+Pixel2203@users.noreply.github.com>
2023-10-13 12:26:03 +0200

    Update ZirconItem.java

--- src/main/java/com/example/examplemod/item/custom/ZirconItem.java
+++ src/main/java/com/example/examplemod/item/custom/ZirconItem.java
@@ -1,22 +1,17 @@
 package com.example.examplemod.item.custom;
 
-import net.minecraft.client.KeyMapping;
-import net.minecraft.client.gui.Gui;
+
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.core.particles.SimpleParticleType;
-import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.entity.Display;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.client.event.ScreenEvent;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
@@ -33,15 +28,6 @@ public class ZirconItem extends Item {
         level.addParticle(ParticleTypes.BUBBLE , player.position().x() , player.position().y() , player.position().z()
                 , 0,0,0);
         return super.use(level, player, hand);
-    }
-    @Override
-    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
-        pTooltipComponents.add(Component.translatable("tooltip.pixelcraft.itemdecs1.tooltip"));
-
-        while (pLevel.isDay()) {
-            pTooltipComponents.add(Component.translatable("tooltip.pixelcraft.itemdecs2.tooltip"));
-            pTooltipComponents.add(Component.translatable("tooltip.pixelcraft.itemdecs3.tooltip"));}
-        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
     }
 
 
