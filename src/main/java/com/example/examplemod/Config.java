24b2697330911385bed172f213fe43bbc597477d
Marvin Kaiser <marvin.kaiser@gmail.com>
2024-07-25 18:02:29 +0200

    added logs

--- src/main/java/com/example/examplemod/Config.java
+++ src/main/java/com/example/examplemod/Config.java
@@ -1,5 +1,8 @@
 package com.example.examplemod;
 
+import com.example.examplemod.block.BlockRegistry;
+import com.example.examplemod.entity.EntityRegistry;
+import com.example.examplemod.item.ItemRegistry;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.common.ForgeConfigSpec;
@@ -7,6 +10,8 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.config.ModConfigEvent;
 import net.minecraftforge.registries.ForgeRegistries;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Collections;
 import java.util.List;
@@ -20,28 +25,14 @@ public class Config
 {
     private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
 
-    private static final ForgeConfigSpec.BooleanValue LOG_DIRT_BLOCK = BUILDER
-            .comment("Whether to log the dirt block on common setup")
-            .define("logDirtBlock", true);
-
-    private static final ForgeConfigSpec.IntValue MAGIC_NUMBER = BUILDER
-            .comment("A magic number")
-            .defineInRange("magicNumber", 42, 0, Integer.MAX_VALUE);
-
-    public static final ForgeConfigSpec.ConfigValue<String> MAGIC_NUMBER_INTRODUCTION = BUILDER
-            .comment("What you want the introduction message to be for the magic number")
-            .define("magicNumberIntroduction", "The magic number is... ");
-
     // a list of strings that are treated as resource locations for items
     private static final ForgeConfigSpec.ConfigValue<List<? extends String>> ITEM_STRINGS = BUILDER
             .comment("A list of items to log on common setup.")
             .defineListAllowEmpty("items", List.of("minecraft:iron_ingot"), Config::validateItemName);
 
     static final ForgeConfigSpec SPEC = BUILDER.build();
+    private static final Logger log = LoggerFactory.getLogger(Config.class);
 
-    public static boolean logDirtBlock;
-    public static int magicNumber;
-    public static String magicNumberIntroduction;
     public static Set<Item> items;
 
     private static boolean validateItemName(final Object obj)
@@ -52,13 +43,8 @@ public class Config
     @SubscribeEvent
     static void onLoad(final ModConfigEvent event)
     {
-        logDirtBlock = LOG_DIRT_BLOCK.get();
-        magicNumber = MAGIC_NUMBER.get();
-        magicNumberIntroduction = MAGIC_NUMBER_INTRODUCTION.get();
-
-        // convert the list of strings into a set of items
-        items = ITEM_STRINGS.get().stream()
-                .map(itemName -> ForgeRegistries.ITEMS.getValue(new ResourceLocation(itemName)))
-                .collect(Collectors.toSet());
+        ItemRegistry.ITEMS.getEntries().stream().map(registeredItem -> registeredItem.getId().toString()).forEach(s -> log.info("Registered Item: {}", s));
+        EntityRegistry.ENTITIES.getEntries().stream().map(registeredItem -> registeredItem.getId().toString()).forEach(s -> log.info("Registered Entity: {}", s));
+        BlockRegistry.BLOCKS.getEntries().stream().map(registeredItem -> registeredItem.getId().toString()).forEach(s -> log.info("Registered Block: {}", s));
     }
 }
